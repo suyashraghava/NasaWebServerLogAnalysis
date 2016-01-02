afd83f229af60e2171d6baf002d4f63d03f3f141
System Administrator <root@abcs-MacBook-Pro.local>
2016-01-02 02:25:54 +0000

    removed

--- Task4/Task4_2/Reducer2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import sys
-
-for line in sys.stdin:
-    line = line.strip().split('\t')
-    uid = line[1].split()
-
-    print uid[0],'\t','->','\t',uid[1]
-
--- Task4/Task4_3/Reducer2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import sys
-
-for line in sys.stdin:
-    line = line.strip().split('\t')
-
-    uid = line[0].split()
-    print uid[1],'\t','->',uid[0],'\t',uid[2]
